e0783ae6364196eecc8edd1490bfd4aa14067beb
manjumadalabhanvi <mmadalabhanvi@gmail.com>
2025-11-29 19:44:37 +0530

    Initial project setup with React + Vite + Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-4xl'>Welcome to the  shoppyGlobe Website!</h1>
+    </div>
+  )
+}
+
+export default App
